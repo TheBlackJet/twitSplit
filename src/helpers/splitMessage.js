544c08d91951d2c6de5806727db7ecdf2efd8c27
Khoa Do <happyboy0412@gmail.com>
2017-07-06 23:10:33 +0700

    add more test => fix some bugs based on new tests => add code comment

--- src/helpers/splitMessage.js
+++ src/helpers/splitMessage.js
@@ -1,55 +1,97 @@
 const MAX_SENT_LEN = 50;
 
-function splitMessage(msg) {
+/**
+ * Pad "0" to number to create a string
+ * Ex: 1 with size 3 => "001"
+ * @param {number} num 
+ * @param {number} size 
+ */
+function pad(num, size) {
+	var s = num + "";
+	while (s.length < size) s = "0" + s;
+	return s;
+}
 
+/**
+ * @param {string} msg - The input string
+ * @return {array<string>} - The array of splitting messages
+ * If the input one does not need to split, this function will return an 
+ * array of one item. 
+ */
+function splitMessage(msg) {
+	// nmsg : normalized message
 	const nmsg = msg.trim();
   
+	/* from here we will work only with the normalized one */
+
   const msgLen = nmsg.length;
-  
+  // console.log('msglen:', msgLen);
+
+	// first if len is on range => return
   if (msgLen <= MAX_SENT_LEN) {
   	return [nmsg];
   }
   
+	// next calculate the len of indicator
+	// idea is:
+	// + we start with range [1, 9] => 1 digit
+	// + this means indicator length is {digit(s) * 2 + 1}
+	// + example: "1/2" => length is 3
+	// + now the rest is for msg's text => MAX_SENT_LEN - 3
+	// + in this case is 50 - 3 = 47
+	// + now with this 47 how many chunk we need?
+	// + let message's length / 47
+	// + if result is (maybe) 8 => well good to go
+	// + if result is (maybe) 15 => wow, not good anymore
+	// + because we will need 2*2+1 = 5 for indicator's length
+	// + => increase the digit to 2
+	// + => loop back again
   let digits = 1;
   let requiredDigits = 0;
   let indicatorLen = 0;
-  let blockLen = 0;
+  let chunkLen = 0;
   
-  while(digits > requiredDigits) {
-    indicatorLen = digits * 2 + 1
+  do {
+    indicatorLen = digits * 2 + 1;
 
-    blockLen = MAX_SENT_LEN - indicatorLen;
+    chunkLen = MAX_SENT_LEN - indicatorLen;
 
-    let estimate = msgLen / blockLen;
-
-    let integer = Math.floor(estimate);
+    let integer = Math.round(msgLen / chunkLen);
     requiredDigits = integer.toString(10).length;
+		// console.log('required:', requiredDigits);
     
-    if (digits > requiredDigits)
+    if (digits < requiredDigits)
       digits++;
   }
+	while(digits < requiredDigits)
+	
+	chunkLen = MAX_SENT_LEN - (digits*2+1);
+	// console.log('chunkLen:', chunkLen);
   
-  
-  let blockCount = 0;
-  let blocks = [];
+	// because we only split by white-space
+	// we will split to get words
   const words = nmsg.split(' ');
+  let chunkCount = 0;
+  let chunks = [];
   
+	// for each word we put to chunk until full
+	// then we make new chunk and add to new one
 	try {
 		words.forEach(word => {
 			if (word.length > MAX_SENT_LEN) {
-				throw 'error: there is a word over 50 letters!';
+				throw '<error>: there is a word over 50 letters!';
 			}
 
-			if (blockCount === 0 || blocks[blockCount-1].length + word.length > blockLen) {
-				blocks.push('');
-				blockCount++;
+			if (chunkCount === 0 || chunks[chunkCount-1].length + word.length + 1 > chunkLen) {
+				chunks.push('');
+				chunkCount++;
 			}
 			
-			blocks[blockCount-1] = `${blocks[blockCount-1]} ${word}`;
+			chunks[chunkCount-1] = `${chunks[chunkCount-1]} ${word}`;
 		});
 		
-		blocks = blocks.map((block, index) => `${index+1}/${blocks.length}${block}`);
-		return blocks;
+		chunks = chunks.map((block, index) => `${pad(index+1, requiredDigits)}/${chunks.length}${block}`);
+		return chunks;
 	}
 	catch(e) {
 		return [e];
--- src/helpers/splitMessage.spec.js
+++ src/helpers/splitMessage.spec.js
@@ -11,13 +11,48 @@ const cases = [
 		in: 'I can\'t believe Tweeter now supports chunking my messages, so I don\'t have to do it myself.',
 		out: [
 			'1/2 I can\'t believe Tweeter now supports chunking',
-			'2/2 my messages, so I don\'t have to do it myself.'
+			'2/2 my messages, so I don\'t have to do it myself.',
 		]
 	},
 	{
 		what: 'If the message contains a span of non-whitespace characters longer than 50 characters, display an error',
 		in: 'abc def 12345678901234567890123456789012345678901234567890XYZ ghi',
-		out: ['error: there is a word over 50 letters!']
+		out: ['<error>: there is a word over 50 letters!']
+	},
+	{
+		what: 'More splitting - case 1',
+		in: 'Pariatur commodo dolor dolore aliquip occaecat ut eu cupidatat culpa.',
+		out: [
+			'1/2 Pariatur commodo dolor dolore aliquip occaecat',
+			'2/2 ut eu cupidatat culpa.',
+		]
+	},
+	{
+		what: 'More splitting - case 2',
+		in: 'Nisi Lorem cillum magna veniam ea veniam anim nisi amet tempor cupidatat laborum dolor in labore reprehenderit.',
+		out: [
+			'1/3 Nisi Lorem cillum magna veniam ea veniam anim',
+			'2/3 nisi amet tempor cupidatat laborum dolor in',
+			'3/3 labore reprehenderit.',
+		]
+	}	,
+	{
+		what: 'Case Paragraph => 2 digit',
+		in: 'Occaecat labore deserunt ipsum amet ipsum. Laborum in proident cupidatat aute Lorem sunt veniam adipisicing commodo aute voluptate. Qui incididunt eu ut velit mollit non fugiat. Velit ea exercitation aliquip minim sunt esse enim. Ea amet velit aute occaecat nostrud. Laboris pariatur ut elit ut ut ipsum. Consectetur consequat ad incididunt Lorem et aliquip commodo. Fugiat ut Lorem eu minim amet fugiat. Officia et dolore mollit labore minim nulla aliquip. Nostrud aliquip est sint irure mollit.',
+		out: [
+			'01/12 Occaecat labore deserunt ipsum amet ipsum.',
+			'02/12 Laborum in proident cupidatat aute Lorem',
+			'03/12 sunt veniam adipisicing commodo aute',
+			'04/12 voluptate. Qui incididunt eu ut velit mollit',
+			'05/12 non fugiat. Velit ea exercitation aliquip',
+			'06/12 minim sunt esse enim. Ea amet velit aute',
+			'07/12 occaecat nostrud. Laboris pariatur ut elit',
+			'08/12 ut ut ipsum. Consectetur consequat ad',
+			'09/12 incididunt Lorem et aliquip commodo. Fugiat',
+			'10/12 ut Lorem eu minim amet fugiat. Officia et',
+			'11/12 dolore mollit labore minim nulla aliquip.',
+			'12/12 Nostrud aliquip est sint irure mollit.'
+		]
 	}
 ];
 
